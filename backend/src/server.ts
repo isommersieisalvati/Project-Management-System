64b82c36dde10339d65074197cddd427997130cc
Fiona <fiona2016@protonmail.com>
2025-08-08 00:12:58 -0700

    Deployment backend.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,11 @@ import helmet from "helmet";
 import path from "path";
 import dotenv from "dotenv";
 
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Load environment variables
 dotenv.config();
 
